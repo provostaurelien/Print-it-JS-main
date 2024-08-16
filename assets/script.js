e1e6a363679170b25101e45b052b9be6f3d8bdb0
Provost Aurelien <Provostaurelien@gmail.com>
2024-08-16 12:28:30 +0200

    Gestion indentation

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,65 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
 // Définition des contantes
 
-let i=0
+let i = 0;
 let flechedroite = document.querySelector("#banner .arrow_right");
 let flechegauche = document.querySelector("#banner .arrow_left");
 const dots = document.querySelectorAll(".dot");
 const img = document.querySelector("#banner .banner-img");
 const texte = document.querySelector("#banner p");
-console.log(texte)
+console.log(texte);
 
 function updateSlide(i) {
-	//Gestion des dots foreach indiqué par ia
-	dots.forEach(dot => dot.classList.remove("dot_selected"));
-	
-	dots[i].classList.add("dot_selected");
+  //Gestion des dots foreach indiqué par ia
+  dots.forEach((dot) => dot.classList.remove("dot_selected"));
 
-	// Changement des images
+  dots[i].classList.add("dot_selected");
 
-	img.src = "./assets/images/slideshow/"+slides[i].image
+  // Changement des images
 
-// Changement de paragraphe
+  img.src = "./assets/images/slideshow/" + slides[i].image;
 
-	texte.innerHTML = slides[i].tagLine
+  // Changement de paragraphe
+
+  texte.innerHTML = slides[i].tagLine;
 }
 
 updateSlide(i);
-// Gestion des flèches du caroussel 
+// Gestion des flèches du caroussel
 
+flechedroite.addEventListener("click", function () {
+  i++;
+  if (i >= slides.length) {
+    i = 0;
+  }
+  console.log(i);
+  updateSlide(i);
+});
 
-
-flechedroite.addEventListener("click", function() {
-	i++
-	if (i>= slides.length) {
-		i= 0
-	}
-	console.log(i)
-	updateSlide(i)
-})
-
-flechegauche.addEventListener("click", function() {
-	i--
-	if (i===-1) {
-		i= slides.length -1
-	}
-	console.log(i)
-	updateSlide(i)
-})
-
-
+flechegauche.addEventListener("click", function () {
+  i--;
+  if (i === -1) {
+    i = slides.length - 1;
+  }
+  console.log(i);
+  updateSlide(i);
+});
